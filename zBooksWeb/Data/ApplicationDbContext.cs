f6cb84b4de2da1f5df2a1a4ccf446713b56aebab
z-shaffer <zshaffer@protonmail.com>
2024-03-09 01:04:19 +0100

    Create ApplicationDbContext.cs

---
+++ zBooksWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using zBooksWeb.Models;
+
+namespace zBooksWeb.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Category> CategoriesType { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+            new Category { Id = 2, Name = "Sci-Fi", DisplayOrder = 2 },
+            new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+        );
+    }
+}
