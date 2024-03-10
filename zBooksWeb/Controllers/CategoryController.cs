588df970e4cb4bfff61445e457d5dad8263882ae
z-shaffer <zshaffer@protonmail.com>
2024-03-10 21:24:58 +0100

    Building category page

--- zBooksWeb/Controllers/CategoryController.cs
+++ zBooksWeb/Controllers/CategoryController.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using zBooksWeb.Data;
+using zBooksWeb.Models;
 
 namespace zBooksWeb.Controllers;
 
-public class CategoryController : Controller
+public class CategoryController(ApplicationDbContext db) : Controller
 {
+    private readonly ApplicationDbContext _db = db;
+
     public IActionResult Index()
     {
-        return View();
+        List<Category> objCategoryList = _db.Categories.ToList();
+        return View(objCategoryList);
     }
 }
