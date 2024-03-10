e46688ac06ea6af68d3ceca6a7e7279213cee356
z-shaffer <zshaffer@protonmail.com>
2024-03-10 21:58:26 +0100

    Create new category init

--- zBooksWeb/Controllers/CategoryController.cs
+++ zBooksWeb/Controllers/CategoryController.cs
@@ -13,4 +13,9 @@ public class CategoryController(ApplicationDbContext db) : Controller
         List<Category> objCategoryList = _db.Categories.ToList();
         return View(objCategoryList);
     }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
 }
