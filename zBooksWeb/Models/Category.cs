11fc677d23a003bd436e050ebdc6dc5eb65b8cbb
z-shaffer <zshaffer@protonmail.com>
2024-03-12 15:55:49 +0100

    More specific input standards

--- zBooksWeb/Models/Category.cs
+++ zBooksWeb/Models/Category.cs
@@ -8,8 +8,10 @@ public class Category
     [Key]
     public int Id { get; set; }
     [Required]
+    [MaxLength(30)]
     [DisplayName("Category Name")]
     public string Name { get; set; }
     [DisplayName("Display Order")]
+    [Range(1,100,ErrorMessage = "Display Order must be a value between 1 and 100.")]
     public int DisplayOrder { get; set; }
 }
