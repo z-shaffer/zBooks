833d4807feecc24c710cead28e5c8635b53b35c2
z-shaffer <zshaffer@protonmail.com>
2024-03-16 22:21:53 +0100

    Repository pattern init

---
+++ zBooks.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,13 @@
+using System.Linq.Expressions;
+
+namespace zBooks.DataAccess.Repository.IRepository;
+
+public interface IRepository<T> where T : class
+{
+    //T - Category
+    IEnumerable<T> GetAll();
+    T Get(Expression<Func<T, bool>> filter);
+    void Add(T entity);
+    void Remove(T entity);
+    void RemoveRange(IEnumerable<T> entity);
+}
---
+++ zBooks.DataAccess/Repository/Repository.cs
@@ -0,0 +1,47 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using zBooks.DataAccess.Data;
+using zBooks.DataAccess.Repository.IRepository;
+
+namespace zBooks.DataAccess.Repository;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly ApplicationDbContext _db;
+    internal DbSet<T> dbSet;
+
+    public Repository(ApplicationDbContext db)
+    {
+        _db = db;
+        this.dbSet = _db.Set<T>();
+        //_db.Categories == dbSet
+    }
+    
+    public IEnumerable<T> GetAll()
+    {
+        IQueryable<T> query = dbSet;
+        return query.ToList();
+    }
+
+    public T Get(Expression<Func<T, bool>> filter)
+    {
+        IQueryable<T> query = dbSet;
+        query = query.Where(filter);
+        return query.FirstOrDefault();
+    }
+
+    public void Add(T entity)
+    {
+        dbSet.Add(entity);
+    }
+
+    public void Remove(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveRange(IEnumerable<T> entity)
+    {
+        throw new NotImplementedException();
+    }
+}
