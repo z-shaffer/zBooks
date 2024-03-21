7f96e7f795c535eeb77623a7a7ec6497786117fb
z-shaffer <zshaffer@protonmail.com>
2024-03-21 23:22:17 +0100

    UnitOfWork implementation in home controller

--- zBooksWeb/Areas/Customer/Controllers/HomeController.cs
+++ zBooksWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,17 +1,21 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using zBooks.DataAccess.Repository;
+using zBooks.DataAccess.Repository.IRepository;
 using zBooks.Models;
 
 namespace zBooksWeb.Areas.Customer.Controllers;
 
 [Area("Customer")]
-public class HomeController(ILogger<HomeController> logger) : Controller
+public class HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork) : Controller
 {
     private readonly ILogger<HomeController> _logger = logger;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
 
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+        return View(productList);
     }
 
     public IActionResult Privacy()
