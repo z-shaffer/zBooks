536a177b495672d0f949aea00effa5a7e2ab1119
z-shaffer <zshaffer@protonmail.com>
2024-03-21 22:14:44 +0100

    Refactoring into primary constructor

--- zBooksWeb/Areas/Customer/Controllers/HomeController.cs
+++ zBooksWeb/Areas/Customer/Controllers/HomeController.cs
@@ -5,14 +5,9 @@ using zBooks.Models;
 namespace zBooksWeb.Areas.Customer.Controllers;
 
 [Area("Customer")]
-public class HomeController : Controller
+public class HomeController(ILogger<HomeController> logger) : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-    
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    private readonly ILogger<HomeController> _logger = logger;
 
     public IActionResult Index()
     {
