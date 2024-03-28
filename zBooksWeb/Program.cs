f500fa9268c0dc72f71fe683a60d5b99e411b138
z-shaffer <zshaffer@protonmail.com>
2024-03-28 21:07:32 +0100

    shopping cart dependency injection

--- zBooksWeb/Program.cs
+++ zBooksWeb/Program.cs
@@ -27,6 +27,7 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkSto
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
+builder.Services.AddScoped<IShoppingCartManager, ShoppingCartManager>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
