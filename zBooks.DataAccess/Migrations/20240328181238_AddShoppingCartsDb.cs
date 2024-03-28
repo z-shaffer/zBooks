bbc89d73a8bded5e2964028e8ae20399d22a8fa9
z-shaffer <zshaffer@protonmail.com>
2024-03-28 21:09:21 +0100

    AddShoppingCartToDB migration

---
+++ zBooks.DataAccess/Migrations/20240328181238_AddShoppingCartsDb.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace zBooks.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShoppingCartsDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCartItems_Products_ProductId",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ShoppingCartItems",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.DropColumn(
+                name: "ItemId",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.DropColumn(
+                name: "CartId",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.RenameTable(
+                name: "ShoppingCartItems",
+                newName: "CartItems");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ShoppingCartItems_ProductId",
+                table: "CartItems",
+                newName: "IX_CartItems_ProductId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "CartItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ShoppingCartId",
+                table: "CartItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "ShoppingCarts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingCarts", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_ShoppingCartId",
+                table: "CartItems",
+                column: "ShoppingCartId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Products_ProductId",
+                table: "CartItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_ShoppingCarts_ShoppingCartId",
+                table: "CartItems",
+                column: "ShoppingCartId",
+                principalTable: "ShoppingCarts",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Products_ProductId",
+                table: "CartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_ShoppingCarts_ShoppingCartId",
+                table: "CartItems");
+
+            migrationBuilder.DropTable(
+                name: "ShoppingCarts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartItems_ShoppingCartId",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "ShoppingCartId",
+                table: "CartItems");
+
+            migrationBuilder.RenameTable(
+                name: "CartItems",
+                newName: "ShoppingCartItems");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CartItems_ProductId",
+                table: "ShoppingCartItems",
+                newName: "IX_ShoppingCartItems_ProductId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ItemId",
+                table: "ShoppingCartItems",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CartId",
+                table: "ShoppingCartItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "ShoppingCartItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ShoppingCartItems",
+                table: "ShoppingCartItems",
+                column: "ItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCartItems_Products_ProductId",
+                table: "ShoppingCartItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
