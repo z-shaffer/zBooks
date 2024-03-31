b321f1cdf7163e494169f3883c86a3437c5eeb1d
z-shaffer <zshaffer@protonmail.com>
2024-03-31 01:58:30 +0100

    AddFKCartIdToCartItems migration

---
+++ zBooks.DataAccess/Migrations/20240331002432_AddFKCartIdToCartItems.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace zBooks.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFKCartIdToCartItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Products_ProductId",
+                table: "CartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_ShoppingCarts_ShoppingCartId",
+                table: "CartItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartItems_ProductId",
+                table: "CartItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartItems_ShoppingCartId",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "ShoppingCartId",
+                table: "CartItems");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ShoppingCartId",
+                table: "CartItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_ProductId",
+                table: "CartItems",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_ShoppingCartId",
+                table: "CartItems",
+                column: "ShoppingCartId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Products_ProductId",
+                table: "CartItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_ShoppingCarts_ShoppingCartId",
+                table: "CartItems",
+                column: "ShoppingCartId",
+                principalTable: "ShoppingCarts",
+                principalColumn: "Id");
+        }
+    }
+}
