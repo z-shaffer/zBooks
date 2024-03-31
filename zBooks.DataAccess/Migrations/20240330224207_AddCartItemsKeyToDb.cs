aac3cb6984a7591e6e352ccea78a23c85ece2be0
z-shaffer <zshaffer@protonmail.com>
2024-03-31 01:58:09 +0100

    AddCartItemsKeyToDb migration

---
+++ zBooks.DataAccess/Migrations/20240330224207_AddCartItemsKeyToDb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace zBooks.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCartItemsKeyToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CartId",
+                table: "CartItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CartId",
+                table: "CartItems");
+        }
+    }
+}
